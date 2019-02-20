ee406ff84417afdb47f4d2ec7d23e3ab5cae41b2
TahaKhz <taha.khazaee@gmail.com>
2019-02-20 16:37:23 +0330

    add link to text

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function GetStorage() {
   let result = 0;
   if (typeof localStorage.getItem("Close_app_Suggestion") !== "undefined")
-    result = localStorage.getItem("Close_app_Suggestion");  
+    result = localStorage.getItem("Close_app_Suggestion");
   return result;
 }
-$(document).on("click", "#CloseSuggestion", function() {  
+$(document).on("click", "#CloseSuggestion", function() {
   $("#SuggestionForMobileApp").remove();
   localStorage.setItem("Close_app_Suggestion", "1");
 });
@@ -48,14 +48,14 @@ function BuildDownloadUrl() {
 }
 function BuildHtml() {
   let result =
-    '<div id="SuggestionForMobileApp" style="height: 50px;">' +
-    '<div style="position: absolute;width: 100%;top: 53px;padding: 0;font-size: 15px;padding: 8px;color: rgba(255,111,104,0.7);">' +
-    "<p>" +
-    '<i id="CloseSuggestion" class="close icon-navigate_cross" style="cursor:pointer;position:relative;float: right;font-size: 30px;margin-left: 10px;margin-top: -10px;"></i>' +
-    "با دانلود اپلیکیشن آسان تر خرید کنید ." +
-    '<a style="cursor:pointer !important;" href="' +
+    '<div id="SuggestionForMobileApp" style="height: 65px;">' +
+    '<div style="position: absolute;width: 100%;top: 53px;padding: 0;font-size: 15px;padding: 8px;background: rgba(18,29,44,0.7);">' +
+    "<p style='padding-top:7px'>" +
+    '<i id="CloseSuggestion" class="close icon-navigate_cross" style="cursor:pointer;position:relative;float: right;font-size: 30px;margin-left: 10px;margin-top: -10px;color:#fff;"></i>' +
+    '<a style="cursor:pointer !important; color:#fff;" href="' +
     BuildDownloadUrl() +
     '">' +
+    "با دانلود اپلیکیشن آسان تر خرید کنید ." +
     '<img src="/content/files/content/favicon.png" style="height: 35px;float: left;margin-left: 20px;margin-bottom: 10px;margin-top: -5px;border: 1px solid;padding: 3px;border-radius: 5px;">' +
     "</a>" +
     "</p>" +
@@ -67,6 +67,5 @@ function SuggestApp() {
   $("body").prepend(BuildHtml());
 }
 $(function() {
-  if (DetectMobile())
-    if (GetStorage() != "1") SuggestApp();
+  if (DetectMobile()) if (GetStorage() != "1") SuggestApp();
 });
