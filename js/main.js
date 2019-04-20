9eecb71a3b68200e9ecd67de67b0a6d272dd8f6b
TahaKhz <taha.khazaee@gmail.com>
2019-04-20 13:10:38 +0430

    add method to manage remote connections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+$('input').on('change', function () {
+    if (document.getElementById('yes').checked) {
+        localStorage.setItem("Remote", "yes");
+    } else if (document.getElementById('no').checked) {
+        localStorage.setItem("Remote", "no");
+    }
+});
+$(function () {
+    let result = localStorage.getItem("Remote");
+    if (result == "yes") {
+        $("#yes").prop("checked", true);
+    } else {
+        $("#no").prop("checked", true);
+    }
+});
+
